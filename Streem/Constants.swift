6c2bb8da103caeabfca1d4ae3de009c0d3daadb2
Robert Hopson <robert@streem.pro>
2018-09-11 13:01:54 -0700

    StreemNow test app

---
+++ Streem/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  StreemNow
+//
+//  Created by Sean Adkinson on 7/18/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+let statsEnabled: ObjCBool = false
+let audioEnabled: ObjCBool = false
