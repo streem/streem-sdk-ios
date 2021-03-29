61a2a20cb5babf27a3309f460fb6a7e8fa76c3d6
Jordan Grund <jordan.grund@streem.com>
2021-03-29 15:17:14 -0700

    update auth flow

---
+++ StreemNow/AuthPersister.swift
@@ -0,0 +1,9 @@
+//
+//  AuthPersister.swift
+//  StreemNow
+//
+//  Created by Jordan Grund on 3/29/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
---
+++ StreemNow/OpenIdHelper.swift
@@ -0,0 +1,9 @@
+//
+//  OpenIdHelper.swift
+//  StreemNow
+//
+//  Created by Jordan Grund on 3/29/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
